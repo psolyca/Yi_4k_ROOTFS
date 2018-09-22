cdcb79da3f97d208ad58b9edcb863b59f8f3875c
Damien Gaignon <damien.gaignon@gmail.com>
2018-09-22 11:26:13 +0200

    Fix usb console script
    This fix serial communication through USB.
    The script could be launch at start with bootcmd.sh

--- 1.10.9/usr/local/share/script/usb_console.sh
+++ 1.10.9/usr/local/share/script/usb_console.sh
@@ -3,11 +3,11 @@ killall syslogd
 echo device > /proc/ambarella/usbphy0
 modprobe usbcore
 modprobe ehci-hcd
-modprobe ohci-hcd
+#modprobe ohci-hcd
 modprobe udc-core
 modprobe ambarella_udc
 modprobe libcomposite
 modprobe g_serial
-/sbin/getty -n -L 115200 /dev/ttyGS0 &
+su root -c "/sbin/getty -n -L 115200 /dev/ttyGS0 &"
 klogd
 syslogd -O /dev/ttyGS0
