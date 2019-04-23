2c3e3b769080ef9ab873b35ba948f84d740da1fb
Damien Gaignon <damien.gaignon@gmail.com>
2019-04-23 10:19:53 +0200

    Add ethernet over USB gadget module
    This will enable the ability to connect over USB to an host and avoid wifi.

--- 1.10.9/usr/local/share/script/usb_ether.sh
+++ 1.10.9/usr/local/share/script/usb_ether.sh
@@ -3,12 +3,12 @@
 echo device > /proc/ambarella/usbphy0
 modprobe usbcore
 modprobe ehci-hcd
-modprobe ohci-hcd
+#modprobe ohci-hcd
 modprobe udc-core
 modprobe ambarella_udc
 modprobe libcomposite
 modprobe g_ether
-ifconfig usb0 up
+ifconfig usb0 192.168.40.1 netmask 255.255.255.0 up
 
 echo "\"ifconfig usb0 your_ip\" after host detects usb ethernet device."
 
