e5be259214e88bcb2690052144dfad9c650dc860
Damien Gaignon <damien.gaignon@gmail.com>
2020-09-08 11:33:45 +0200

    Rewrite wifi_start.sh

--- 1.10.9/usr/local/share/script/wifi_start.sh
+++ 1.10.9/usr/local/share/script/wifi_start.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 
-if [ "${1}" == "fast" ]; then
-    if [ -e /sys/module/bcmdhd ]; then
-        wl up
-    fi
-
-    /tmp/wifi_start.sh && exit 0
-fi
-
+export SCRIPT_PATH=/usr/local/share/script
 export WIFI_CONFIGURE_PATH="/tmp/wifi.conf"
 
 SYNC_CONFIG ()
@@ -21,7 +14,7 @@ SYNC_CONFIG ()
     # system (should already exists with wifi_configure.sh
     if [ ! -e ${WIFI_CONFIGURE_PATH} ]; then
         echo "Load wifi.conf from system ..."
-        cp -rf /usr/local/share/script/wifi.conf ${WIFI_CONFIGURE_PATH}
+        cp -rf ${SCRIPT_PATH}/wifi.conf ${WIFI_CONFIGURE_PATH}
     fi
 
     # FL0
@@ -54,34 +47,44 @@ SYNC_CONFIG ()
 
 wait_mmc_add ()
 {
-    /usr/local/share/script/t_gpio.sh ${WIFI_EN_GPIO} 0
-    /usr/local/share/script/t_gpio.sh ${WIFI_EN_GPIO} 1
-    if [ -e /proc/ambarella/mmc_fixed_cd ]; then
-        mmci=`grep mmc /proc/ambarella/mmc_fixed_cd |awk $'{print $1}'|cut -c 4|tail -n 1`
-        echo "${mmci} 1" > /proc/ambarella/mmc_fixed_cd
-    else
-        echo 1 > /sys/module/ambarella_config/parameters/sd1_slot0_fixed_cd
-    fi
+    ${SCRIPT_PATH}/t_gpio.sh ${WIFI_EN_GPIO} 0
+    ${SCRIPT_PATH}/t_gpio.sh ${WIFI_EN_GPIO} 1
+
+    mmci=`grep mmc /proc/ambarella/mmc_fixed_cd |awk $'{print $1}'|cut -c 4|tail -n 1`
+    echo "${mmci} 1" > /proc/ambarella/mmc_fixed_cd
+
     echo 24000000 > /sys/module/ambarella_sd/parameters/sdio_host_max_frequency
     echo 24000000 > /sys/kernel/debug/mmc0/clock
-    n=0
-    while [ -z "`ls /sys/bus/sdio/devices`" ] && [ $n -ne 30 ]; do
-        n=$(($n + 1))
+
+    local fWaiting=0
+    while [ -z "`ls /sys/bus/sdio/devices`" ] && [ $fWaiting -le 30 ]; do
+        fWaiting=$(($fWaiting + 1))
         sleep 0.1
     done
+
+    if [ $fWaiting -eq 30 ]; then
+        return 1
+    else
+        return 0
+    fi
 }
 
 wait_wlan0 ()
 {
-    n=0
-    ifconfig wlan0
-    waitagain=$?
-    while [ $waitagain -ne 0 ] && [ $n -ne 60 ]; do
-        n=$(($n + 1))
-        sleep 0.1
+    local fWaiting=0
+    local fData=1
+    while [ $fData -ne 0 ] && [ $fWaiting -lt 60 ]; do
+        fWaiting=$(($fWaiting + 1))
         ifconfig wlan0
-        waitagain=$?
+        fData=$?
+        sleep 0.1
     done
+
+    if [ $fWaiting -eq 60 ]; then
+        return 1
+    else
+        return 0
+    fi
 }
 
 events_manager ()
@@ -89,15 +92,14 @@ events_manager ()
     if [ -e /tmp/fuse_d/events ]; then
         sleep 2
         echo "Starting events callback..."
-        /sbin/start-stop-daemon -S -b -p /var/run/eventsCB.pid -m -a /usr/local/share/script/cmdyi.py
+        /sbin/start-stop-daemon -S -b -p /var/run/eventsCB.pid -m -a ${SCRIPT_PATH}/cmdyi.py
     fi
 }
 
 SYNC_CONFIG
 
 if [ "${ETHER_MODE}" == "yes" ]; then
-    echo "Start ethernet over USB"
-    /usr/local/share/script/usb_ether.sh start ${ETHER_IP}
+    /usr/local/share/script/usb_ether.sh start
     if [ "${KEEP_WIFI}" == "no" ]; then
         /usr/bin/SendToRTOS net_ready ${ETHER_IP}
         echo "Wifi will not be started"
@@ -106,56 +108,30 @@ if [ "${ETHER_MODE}" == "yes" ]; then
     fi
 fi
 
-if [ "${WIFI_SWITCH_GPIO}" != "" ]; then
-    WIFI_SWITCH_VALUE=`/usr/local/share/script/t_gpio.sh ${WIFI_SWITCH_GPIO}`
-    echo "GPIO ${WIFI_SWITCH_GPIO} = ${WIFI_SWITCH_VALUE}"
-    if [ "${WIFI_SWITCH_VALUE}" == "0" ]; then
-        #send network turned off to RTOS
-        if [ -x /usr/bin/SendToRTOS ]; then
-            /usr/bin/SendToRTOS net_off
-        elif [ -x /usr/bin/boot_done ]; then
-            boot_done 1 2 1
-        fi
-        exit 0
+if [ -z "`ls /sys/bus/sdio/devices`" ]; then
+    wait_mmc_add
+    if [ $? -ne 0 ]; then
+        echo "MMC devices have not been created"
+        exit 1
     fi
 fi
 
-if [ "${1}" == "ssid" ]; then
-    export    AP_SSID="${2}"
-    export    AP_XY_SSID="${1}"
-    echo "AP_SSID=${AP_SSID}"
-    echo "AP_XY_SSID=${AP_XY_SSID}"
-fi
-if [ "${3}" == "5G" ]; then
-    export    AP_CHANNEL_5G=1
-    echo "AP_CHANNEL_5G=${AP_CHANNEL_5G}"
-elif [ "${3}" == "2.4G" ]; then
-    export    AP_CHANNEL_5G=0
-fi
-if [ -e /sys/module/bcmdhd/parameters/g_txglom_max_agg_num ]; then
-    echo 0 >/sys/module/bcmdhd/parameters/g_txglom_max_agg_num
-fi
-if [ "${WIFI_EN_GPIO}" != "" ] && [ -z "`ls /sys/bus/sdio/devices`" ]; then
-    wait_mmc_add
-fi
-
 #check wifi mode
-/usr/local/share/script/load.sh "${WIFI_MODE}"
+${SCRIPT_PATH}/load.sh "${WIFI_MODE}"
 
-waitagain=1
-if [ -n "`ls /sys/bus/sdio/devices`" ] || [ -e /sys/bus/usb/devices/*/net ]; then
+if [ -n "`ls /sys/bus/sdio/devices`" ]; then
     wait_wlan0
-fi
-if [ $waitagain -ne 0 ]; then
-    echo "There is no WIFI interface! pls check wifi driver or fw"
-    exit 1
+    if [ $? -ne 0 ]; then
+        echo "There is no WIFI interface! pls check wifi driver or fw"
+        exit 1
+    fi
 fi
 
 echo "Found  WIFI interface!"
 if [ "${WIFI_MODE}" == "sta" ]; then
-    /usr/local/share/script/sta_start.sh $@
+    ${SCRIPT_PATH}/sta_start.sh $@
 else
-    /usr/local/share/script/ap_start.sh $@
+    ${SCRIPT_PATH}/ap_start.sh $@
 fi
 
 events_manager
