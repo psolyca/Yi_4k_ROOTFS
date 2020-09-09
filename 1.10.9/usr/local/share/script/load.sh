7c57318ace732902d09145f4eca62a782715600f
Damien Gaignon <damien.gaignon@gmail.com>
2020-09-09 23:15:24 +0200

    Rewrite load.sh

--- 1.10.9/usr/local/share/script/load.sh
+++ 1.10.9/usr/local/share/script/load.sh
@@ -8,7 +8,7 @@ fi
 rm -f /tmp/wifi.preloaded
 
 LOG_ENABLE_SCRIPT="/tmp/SD0/save_log_enable.script"
-LOG_OUT_FILE="/tmp/SD0/rtmp_wifi.log"
+LOG_OUT_FILE="/tmp/SD0/load.log"
 
 KO=bcmdhd.ko
 # BCM will get name by configure.
@@ -26,78 +26,11 @@ if [ -e $LOG_ENABLE_SCRIPT  ]; then
     echo "$conf" >> $LOG_OUT_FILE
 fi
 
-load_mod_old()
-{
-    mac=`cat /tmp/wifi0_mac`
-    if [ "${mac}" != "00:00:00:00:00:00" ] &&  [ "${mac}" != "" ]; then
-        insmod /lib/modules/${KO} ${P_FW}_apsta.bin ${P_NVRAM} ${P_IF} ${P_DBG} $1 amba_initmac=${mac}
-    else
-        insmod /lib/modules/${KO} ${P_FW}_apsta.bin ${P_NVRAM} ${P_IF} ${P_DBG} $1
-    fi
-}
-
 load_mod()
 {
     echo "load_mod > MAC = ${WIFI_MAC}    CHIP = ${CHIP_TYPE}"
 
-    if [ "${CHIP_TYPE}" == "43455" ] && [ -e /usr/local/bcmdhd/fw_apsta43455.bin ]; then
-        insmod /lib/modules/${KO} ${P_FW}_apsta43455.bin ${P_NVRAM}ram43455.txt ${P_IF} ${P_DBG} $1 amba_initmac=${WIFI_MAC}
-    elif [ -e /usr/local/bcmdhd/fw_apsta43340.bin ]; then
-        insmod /lib/modules/${KO} ${P_FW}_apsta43340.bin ${P_NVRAM}ram43340.txt ${P_IF} ${P_DBG} $1 amba_initmac=${WIFI_MAC}
-    else
-        insmod /lib/modules/${KO} ${P_FW}_apsta.bin ${P_NVRAM}ram.txt ${P_IF} ${P_DBG} $1 amba_initmac=${WIFI_MAC}
-    fi
-}
-
-load_mod_tmp()
-{
-    if [ "${mac}" == "58:70:C6:00:00:00" ]; then
-        mac=`cat /tmp/wifi0_mac`
-    fi
-    if [ "${mac}" != "00:00:00:00:00:00" ] &&  [ "${mac}" != "" ]; then
-        if [ -e /tmp/${KO} ]; then
-            if [ -e /tmp/fw_apsta.bin ]; then
-                if [ -e /tmp/nvram.txt ]; then
-                    echo "load /tmp/ko /tmp/wifi_fw /tmp/nvram.txt"
-                    insmod /tmp/${KO} ${P_FW_SD}_apsta.bin ${P_NVRAM_SD} ${P_IF} ${P_DBG} $1 amba_initmac=${mac}
-                else
-                    echo "load /tmp/ko /tmp/wifi_fw /user/loacl/bcmdhd/nvram.txt"
-                    insmod /tmp/${KO} ${P_FW_SD}_apsta.bin ${P_NVRAM} ${P_IF} ${P_DBG} $1 amba_initmac=${mac}
-                fi
-            else
-                if [ -e /tmp/nvram.txt ]; then
-                    echo "load /tmp/ko /user/local/wifi_fw /tmp/nvram.txt"
-                    insmod /tmp/${KO} ${P_FW}_apsta.bin ${P_NVRAM_SD} ${P_IF} ${P_DBG} $1 amba_initmac=${mac}
-                else
-                    echo "load /tmp/ko /user/loacl/bcmdhd/nvram.txt & fw wifi"
-                    insmod /tmp/${KO} ${P_FW}_apsta.bin ${P_NVRAM} ${P_IF} ${P_DBG} $1 amba_initmac=${mac}
-                fi
-            fi
-        else
-            if [ -e /tmp/fw_apsta.bin ]; then
-                if [ -e /tmp/nvram.txt ]; then
-                    echo "load /lib/mudules/ko /tmp/wifi_fw /tmp/nvram.txt"
-                    insmod /lib/modules/${KO} ${P_FW_SD}_apsta.bin ${P_NVRAM_SD} ${P_IF} ${P_DBG} $1 amba_initmac=${mac}
-                else
-                    echo "load /lib/modules/ko /tmp/wifi_fw /user/loacl/bcmdhd/nvram.txt"
-                    insmod /lib/modules/${KO} ${P_FW_SD}_apsta.bin ${P_NVRAM} ${P_IF} ${P_DBG} $1 amba_initmac=${mac}
-                fi
-            else
-                echo "load /lib/modules/ko /user/local/bcmdhd/wifi_fw & nvram.txt"
-                insmod /lib/modules/${KO} ${P_FW}_apsta.bin ${P_NVRAM} ${P_IF} ${P_DBG} $1 amba_initmac=${mac}
-            fi
-        fi
-    else
-        if [ -e /tmp/fw_apsta.bin ]; then
-            if [ -e /tmp/nvram.txt ]; then
-                insmod /lib/modules/${KO} ${P_FW_SD}_apsta.bin ${P_NVRAM_SD} ${P_IF} ${P_DBG} $1
-            else
-                insmod /lib/modules/${KO} ${P_FW_SD}_apsta.bin ${P_NVRAM} ${P_IF} ${P_DBG} $1
-            fi
-        else
-            insmod /lib/modules/${KO} ${P_FW}_apsta.bin ${P_NVRAM} ${P_IF} ${P_DBG} $1
-        fi
-    fi
+    insmod /lib/modules/${KO} ${P_FW}_apsta43340.bin ${P_NVRAM}ram43340.txt ${P_IF} ${P_DBG} $1 amba_initmac=${WIFI_MAC}
 }
 
 if [ -e $LOG_ENABLE_SCRIPT  ]; then
